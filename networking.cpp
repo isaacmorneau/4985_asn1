05caa6ab92736998f0f2d0c8f466d48ed06f9647
Isaac <isaac@isaacmorneau.com>
2017-01-22 16:14:11 -0800

    Port By Service functional
    minor output formatting

--- networking.cpp
+++ networking.cpp
@@ -15,26 +15,37 @@ std::string SbP(const std::string &port, const std::string &proto){
 
     WORD wVersionRequested = MAKEWORD(2,2);
     WSADATA wsaData;
-
-    // Open up a Winsock v2.2 session
     WSAStartup(wVersionRequested, &wsaData);
 
     s_port = atoi(port.c_str());
 
     sv = getservbyport (htons(s_port), proto.c_str());
-    if (sv == NULL){
-        result = "Error in getservbyport";
-    } else {
+    if (sv == NULL)
+        sprintf(buffer,"Service not found for port %d",s_port);
+    else
         sprintf(buffer, "The service for %s port %d is: %s", proto.c_str(), s_port, sv->s_name);
-        result = buffer;
-    }
+    result = buffer;
     WSACleanup();
     return result;
 }
 
 std::string PbS(const std::string &service, const std::string &proto){
-    delay(3);
-    return "done";
+    std::string result;
+    char buffer[256];
+    struct servent *sv;
+
+    WORD wVersionRequested = MAKEWORD(2,2);
+    WSADATA wsaData;
+    WSAStartup(wVersionRequested, &wsaData);
+
+    sv = getservbyname (service.c_str(), proto.c_str());
+    if (sv == NULL)
+        sprintf(buffer,"Port not found for service %s", service.c_str());
+    else
+        sprintf (buffer, "The port number for %s is: %d\n", service.c_str(), ntohs(sv->s_port));
+    result = buffer;
+    WSACleanup();
+    return result;
 }
 
 std::string HbI(const std::string &ip){
